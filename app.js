fe74207281262aeb80cdaee0985fd7057dcef24e
MaybeMike1 <57484151+MaybeMike1@users.noreply.github.com>
2021-02-13 14:50:56 +0000

    Starting the app with script

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-var mongoose = require('mongoose')
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 
-app.use(express.static(__dirname))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 mongoose.Promise = Promise
 
 var dbUrl = 'mongodb+srv://admin:sesame@learning-nodejs.cigys.mongodb.net/Learning-NodeJS?retryWrites=true&w=majority'
